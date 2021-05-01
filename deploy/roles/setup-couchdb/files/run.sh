57e28306e29789ece5e5e9c7d6586c261816535b
Liu233w <gh@liu233w.com>
2021-05-02 01:52:45 +1000

    Add docker proxy

--- deploy/roles/setup-couchdb/files/run.sh
+++ deploy/roles/setup-couchdb/files/run.sh
@@ -3,11 +3,11 @@
 set -e
 
 docker run\
-      --name couchdb\
-      --env COUCHDB_USER=${COUCH_DB_USER}\
-      --env COUCHDB_PASSWORD=${COUCH_DB_PASS}\
-      --env COUCHDB_SECRET=${COUCH_DB_SECRET}\
-      --env ERL_FLAGS="-setcookie \"${COUCH_DB_SECRET}\" -name \"couchdb@${HOST_IP}\""\
-      -p 5984:5984\
-      -v /home/couchdb/data:/mnt/data/couchdb\
-      ibmcom/couchdb3:${COUCH_DB_VERSION}
+  --name couchdb\
+  --env COUCHDB_USER=${COUCH_DB_USER} \
+  --env COUCHDB_PASSWORD=${COUCH_DB_PASS} \
+  --env COUCHDB_SECRET=${COUCH_DB_SECRET} \
+  --env ERL_FLAGS="-setcookie \"${COUCH_DB_SECRET}\" -name \"couchdb@${HOST_IP}\"" \
+  -p 5984:5984 \
+  -v /home/couchdb/data:/mnt/data/couchdb \
+  ibmcom/couchdb3:${COUCH_DB_VERSION}
