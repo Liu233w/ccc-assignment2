18522e31a614aab9ae638d30d4336bdfc51c67a8
Liu233w <gh@liu233w.com>
2021-05-16 17:54:35 +1000

    Add proxy to docker build

--- flaskapp/docker-build.sh
+++ flaskapp/docker-build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-docker build -t liu233w/ccc-assignment2 .
+docker build -t liu233w/ccc-assignment2 \
+  --build-arg http_proxy=http://wwwproxy.unimelb.edu.au:8000/ \
+  --build-arg https_proxy=http://wwwproxy.unimelb.edu.au:8000/ \
+  .
 docker push liu233w/ccc-assignment2
