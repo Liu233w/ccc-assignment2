cf793f80613761a75782810eb09095cd6b49be4e
Liu233w <gh@liu233w.com>
2021-05-16 20:48:24 +1000

    Docker: optimise building process

--- flaskapp/docker-build.sh
+++ flaskapp/docker-build.sh
@@ -3,5 +3,5 @@
 docker build -t liu233w/ccc-assignment2 \
   --build-arg http_proxy=http://wwwproxy.unimelb.edu.au:8000/ \
   --build-arg https_proxy=http://wwwproxy.unimelb.edu.au:8000/ \
-  .
-docker push liu233w/ccc-assignment2
+  . \
+&& docker push liu233w/ccc-assignment2
