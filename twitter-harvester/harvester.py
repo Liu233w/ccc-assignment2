bc8d07d4da5ced32582db653b67005af8575664c
robert.sloan <robert.sloan@metigy.com>
2021-05-20 23:13:16 +1000

    load and use features in database

--- twitter-harvester/harvester.py
+++ twitter-harvester/harvester.py
@@ -5,7 +5,7 @@ from cloudant.client import CouchDB
 from redis import Redis
 import math
 import api
-from locations import get_features
+from locations import load_features
 from predictor import Predictor, load_model
 
 os.environ["COUCHDB_USERNAME"] = "admin"
@@ -16,39 +16,58 @@ os.environ["MAP_PATH"] = "/Users/robertsloan/repos/ccc-assignment2/flaskapp/fron
 os.environ["MODEL_PATH"] = "/Users/robertsloan/Desktop/BERT_classification_epoch_1.model"
 
 
-def create_params(bounding_box, next_token):
-    x_delta = bounding_box[2] - bounding_box[0]
-    y_delta = bounding_box[3] - bounding_box[1]
+def create_params(feature, next_token, backfill=False):
+
+    # Combine boxes of max 25 miles
+    box = feature["box"]
+    x_delta = box[2] - box[0]
+    y_delta = box[3] - box[1]
     x_total = math.ceil(x_delta / 0.3)
     y_total = math.ceil(y_delta / 0.3)
     x = x_delta / x_total
     y = y_delta / y_total
 
-    bounding_boxes = []
+    boxes = []
     for i in range(x_total):
         for j in range(y_total):
-            bounding_boxes.append([
-                bounding_box[0] + x * i,
-                bounding_box[1] + x * j,
-                bounding_box[0] + x * (i + 1),
-                bounding_box[1] + x * (j + 1),
+            boxes.append([
+                box[0] + x * i,
+                box[1] + y * j,
+                box[0] + x * (i + 1),
+                box[1] + y * (j + 1),
             ])
 
-    bounding_box_query = "(%s)" % " OR ".join(
+    box_query = "(%s)" % " OR ".join(
         map(lambda box: "bounding_box:[%s]" % " ".join(
             map(lambda point: "%.6f" % point, box)
-        ), bounding_boxes))
+        ), boxes))
+
+    # Get the start and end times
+    start_time = None
+    end_time = None
+    if (backfill):
+        start_time = datetime.fromtimestamp(feature["latest"])
+        end_time = start_time + timedelta(days=30)
+    else:
+        end_time = datetime.fromtimestamp(feature["oldest"])
+        start_time = end_time - timedelta(days=30)
+
+    # Keep times within Twitter limits
+    if end_time > datetime.utcnow() - timedelta(seconds=30):
+        end_time = datetime.utcnow() - timedelta(seconds=30)
+    if start_time < datetime(2017, 1, 1):
+        start_time = datetime(2017, 1, 1)
 
     params = {
-        "start_time": "2007-01-01T00:00:00Z",
-        "end_time": (datetime.utcnow() - timedelta(seconds=30)).strftime('%Y-%m-%dT%H:%M:%SZ'),
-        "query": "lang:EN -place:melbourne %s" % bounding_box_query,  # max 1,024 characters
+        "start_time": start_time.strftime('%Y-%m-%dT%H:%M:%SZ'),
+        "end_time": end_time.strftime('%Y-%m-%dT%H:%M:%SZ'),
+        "query": "lang:EN -place:melbourne %s" % box_query,  # max 1,024 characters
         "user.fields": "url",
         "expansions": "geo.place_id,author_id",
         "tweet.fields": "author_id,created_at,geo,id,source,text",
         "place.fields": "full_name,geo,place_type",
         "user.fields": "location,username",
-        "max_results": 10
+        "max_results": 10,
     }
 
     if next_token:
@@ -84,6 +103,58 @@ def format_response(response):
     return tweets
 
 
+def call_for_feature(feature, model, tokenizer, couchdb, redis, backfill=False):
+    response = {"meta": {"next_token": None}}
+    page = 0
+    while "next_token" in response["meta"] and page < 10:
+        next_token = response["meta"]["next_token"]
+        print('Getting tweets for %s, token: %s...' % (feature["name"], next_token))
+        page += 1
+
+        # Get tweets from Twitter
+        gt1 = time()
+        try:
+            response = api.get(
+                endpoint='2/tweets/search/all',
+                params=create_params(feature, next_token, backfill),
+                couchdb=couchdb,
+                redis=redis)
+        except Exception as e:
+            print(e)
+            sleep(1)
+            continue
+        gt2 = time()
+        print("%.2fs to get %s tweets in %s" % (
+            gt2 - gt1,
+            len(response["data"]) if "data" in response else 0,
+            feature["name"]))
+
+        if "data" not in response or len(response["data"]) == 0:
+            continue
+
+        tweets = format_response(response)
+
+        # Add category prediction
+        pt1 = time()
+        for tweet in tweets:
+            predictor = Predictor(
+                tweet["text"], model, tokenizer, threshold=0.55)
+            prediction = predictor.get_tweet_prediction()
+            tweet["category"] = prediction
+        pt2 = time()
+        print("%.2fs to predict" % (pt2 - pt1))
+
+        # Save to CouchDB
+        t1 = time()
+        couchdb["twitter"].bulk_docs(tweets)
+        t2 = time()
+        print("%.2fs to save" % (t2 - t1))
+
+        sleep(1 - max(0, gt2 - t2))
+
+    # TODO: if page >= 10: save url for later
+
+
 def main():
     couchdb = CouchDB(
         user=os.environ["COUCHDB_USERNAME"],
@@ -92,62 +163,30 @@ def main():
         connect=True,
         auto_renew=True)
     redis = Redis(os.environ["REDIS_HOST"], 6379, 0)
-    features = get_features(os.environ["MAP_PATH"])[10:]
-    bert_classification_model_path = os.environ["MODEL_PATH"]
+    load_features(os.environ["MAP_PATH"], couchdb)
 
     # Load classifier
     start_time = time()
-    model, tokenizer = load_model(bert_classification_model_path)
+    model, tokenizer = load_model(os.environ["MODEL_PATH"])
     end_time = time()
     print("Loading Time: %.2fs" % (end_time - start_time))
 
-    for feature in features:
-        response = {"meta": {"next_token": None}}
-        page = 0
-        while "next_token" in response["meta"] and page < 10:
-            next_token = response["meta"]["next_token"]
-            print('Getting tweets with token: %s...' % next_token)
-            page += 1
+    while True:
+        feature = couchdb["features"].get_query_result(
+            selector={"_id": {"$gt": None}},
+            sort=[{"newest": "asc"}],
+            limit=1
+        ).all()[0]
+        call_for_feature(feature, model, tokenizer,
+                         couchdb, redis, backfill=False)
 
-            # Get tweets from Twitter
-            gt1 = time()
-            try:
-                response = api.get(
-                    endpoint='2/tweets/search/all',
-                    params=create_params(feature["box"], next_token),
-                    couchdb=couchdb,
-                    redis=redis)
-            except Exception as e:
-                print(e)
-                sleep(1)
-                continue
-            gt2 = time()
-            print("%.2fs to get %s tweets in %s" % (
-                gt2 - gt1,
-                len(response["data"]) if "data" in response else 0,
-                feature["name"]))
-
-            if "data" not in response or len(response["data"]) == 0:
-                continue
-
-            tweets = format_response(response)
-
-            # Add category prediction
-            pt1 = time()
-            for tweet in tweets:
-                predictor = Predictor(tweet["text"], model, tokenizer, threshold=0.55)
-                prediction = predictor.get_tweet_prediction()
-                tweet["category"] = prediction
-            pt2 = time()
-            print("%.2fs to predict" % (pt2 - pt1))
-
-            # Save to CouchDB
-            t1 = time()
-            couchdb["twitter"].bulk_docs(tweets)
-            t2 = time()
-            print("%.2fs to save" % (t2 - t1))
-
-            sleep(1 - max(0, gt2 - t2))
+        feature_backfill = couchdb["features"].get_query_result(
+            selector={"_id": {"$gt": None}},
+            sort=[{"oldest": "desc"}],
+            limit=1
+        ).all()[0]
+        call_for_feature(feature_backfill, model, tokenizer,
+                         couchdb, redis, backfill=True)
 
 
 if __name__ == "__main__":
--- twitter-harvester/locations.py
+++ twitter-harvester/locations.py
@@ -1,34 +1,62 @@
 import json
+from cloudant.client import CouchDB
+from datetime import datetime
+from hashlib import sha1
 
 
-def get_features(filepath):
-    features = []
+def load_features(filepath, couchdb: CouchDB):
+    if "features" not in couchdb.all_dbs():
+        couchdb.create_database("features", partitioned=False)
+        couchdb["features"].create_query_index(fields=["newest"])
+        couchdb["features"].create_query_index(fields=[{"oldest": "desc"}])
+    poly_features = []
     with open(filepath) as file:
         content = file.read()
         polygons = json.loads(content)
-        features = polygons["features"]
-        for feature in features:
-            coords = feature["geometry"]["coordinates"]
-            box = [None, None, None, None]
-            for x in coords:
-                for y in x:
-                    for z in y:
-                        if box[0] is None or z[0] < box[0]:
-                            box[0] = z[0]
-                        if box[1] is None or z[1] < box[1]:
-                            box[1] = z[1]
-                        if box[2] is None or z[0] > box[2]:
-                            box[2] = z[0]
-                        if box[3] is None or z[1] > box[3]:
-                            box[3] = z[1]
-            feature["bounding_box"] = box
+        poly_features = polygons["features"]
 
-        features = [
-            {
-                "name": feature["properties"]["name"],
-                "box": feature["bounding_box"]
+    features = []
+    for feature in poly_features:
+        coords = feature["geometry"]["coordinates"]
+        box = [None, None, None, None]
+
+        for x in coords:
+            for y in x:
+                for z in y:
+                    if box[0] is None or z[0] < box[0]:
+                        box[0] = z[0]
+                    if box[1] is None or z[1] < box[1]:
+                        box[1] = z[1]
+                    if box[2] is None or z[0] > box[2]:
+                        box[2] = z[0]
+                    if box[3] is None or z[1] > box[3]:
+                        box[3] = z[1]
+
+        id = sha1(json.dumps(box).encode('utf8')).digest().hex()
+        features.append({
+            "id": id,
+            "name": feature["properties"]["name"],
+            "box": box
+        })
+
+    docs = couchdb["features"].get_query_result(
+        selector={
+            "_id": {
+                "$gt": None
             }
-            for feature in features
-        ]
+        }).all()
+
+    known_ids = list(map(lambda doc: doc["_id"], docs))
+    new_features = list(filter(lambda feature: feature["id"] not in known_ids, features))
+
+    timestamp = int(datetime.utcnow().timestamp())
+    new_docs = list(map(lambda feature: {
+        "_id": feature["id"],
+        "box": feature["box"],
+        "name": feature["name"],
+        "newest": timestamp,
+        "oldest": timestamp
+    }, new_features))
+    couchdb["features"].bulk_docs(new_docs)
 
     return features
